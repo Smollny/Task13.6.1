20be85703e85a61529c5327c6a7467ee3149d6e5
Smollny <Smollny@DESKTOP-0HRFRLT>
2022-01-26 23:42:42 +0300

    Добавьте файлы проекта.

---
+++ Task13.6.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Task13._6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var alltext = System.IO.File.ReadAllText("text1.txt").Split(new char[] { ' ', '\n', '\t' });
+            double result1, result2;
+            var stopWatch = Stopwatch.StartNew();
+            var listStrings = new List<string>(alltext);
+            Console.WriteLine($"Создание списка: {result1 = stopWatch.Elapsed.TotalMilliseconds}  мс");
+            
+            stopWatch = Stopwatch.StartNew();
+            var linkedListStrings = new LinkedList<string>(alltext);
+            Console.WriteLine($"Создание связного списка: {result2 = stopWatch.Elapsed.TotalMilliseconds}  мс");
+            Console.WriteLine($"\nСоздание List в {Math.Round(result2 / result1, 2)} раз быстрее, чем LinkedList");
+            Console.ReadKey();
+        }
+    }
+}
